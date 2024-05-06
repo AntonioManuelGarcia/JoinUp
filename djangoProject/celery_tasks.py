31ad38f5a94820f52449f7c641395b523971c521
AntonioManuelGarcia <wiladrow@gmail.com>
2024-05-06 22:47:26 +0200

    celery worker

---
+++ djangoProject/celery_tasks.py
@@ -0,0 +1,13 @@
+# from __future__ import absolute_import
+import os
+
+from celery import Celery
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings.base")
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings1')
+
+app = Celery('djangoProject')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
