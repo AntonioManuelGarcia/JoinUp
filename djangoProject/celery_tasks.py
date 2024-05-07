1da180276ed7ed591cfc13989716fa334e28e7e8
AntonioManuelGarcia <wiladrow@gmail.com>
2024-05-07 04:06:32 +0200

    clean code

--- djangoProject/celery_tasks.py
+++ djangoProject/celery_tasks.py
@@ -4,9 +4,6 @@ from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoProject.settings.local")
 
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject.settings1')
-
 app = Celery('djangoProject')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
